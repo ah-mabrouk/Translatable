8fc485b95dd8d06ea62411a53b707394375c1ddc
Ahmed-Mabrouk <a.mabrouk25@gmail.com>
2023-06-13 06:25:35 +0300

    fix null access

--- src/Helpers/TranslatableHelperFunctions.php
+++ src/Helpers/TranslatableHelperFunctions.php
@@ -3,6 +3,6 @@
 if (! function_exists('translation_id')) {
     function translation_id($model) {
         $locale = request()->locale ?? config('translatable.fallback_locale');
-        return $model->translations->where('locale', $locale)->first()?->id;
+        return $model->translations?->where('locale', $locale)->first()?->id;
     }
 }
